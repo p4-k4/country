14f181778da8f31b640a7ddf2a552456b15400be
Paurini Wiringi <p4k4p4k4@gmail.com>
2022-06-04 04:05:59 +1200

    Update `pubspec.yaml`

--- test/country_test.dart
+++ test/country_test.dart
@@ -3,14 +3,8 @@ import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
+    setUp(() {});
 
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+    test('First Test', () {});
   });
 }
